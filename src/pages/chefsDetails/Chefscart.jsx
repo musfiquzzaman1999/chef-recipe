7e0218a6289e70f5e69d49232f024b6e0d23d490
unknown <musfiquzzama1999@gmail.com>
2023-05-03 01:29:15 +0600

    more data

--- src/pages/chefsDetails/Chefscart.jsx
+++ src/pages/chefsDetails/Chefscart.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 import { AuthContex } from '../../provider/AuthProvider';
 import RecipeCart from './RecipeCart';
+import { FaHeart } from 'react-icons/fa';
 
 const Chefscart = () => {
     const {chefs}=useContext(AuthContex);
@@ -20,15 +21,18 @@ const Chefscart = () => {
     
         return (
             <div>
-               <div className='text-center'>
-                <h1 className='font-semibold text-5xl mt-10'>Welecome to my profile</h1>
-                <h1 className='font-semibold text-4xl mt-10'>Myself {chef?.name}</h1>
-                <div className="avatar">
-  <div className="w-52 rounded-full mt-5">
-    <img src={chef?.picture} />
+             <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row">
+    <img src={chef.picture} className="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 className="text-5xl font-bold"> {chef.name}</h1>
+      <p className="py-6">{chef.bio}</p>
+      <p className='flex text-center'> Likes:<FaHeart className='text-2xl mr-2 ml-2'></FaHeart> {chef.likes}</p>
+      <p>Expreances: {chef.experience} years</p>
+      <p>Total Recipes: {chef.recipes}</p>
+    </div>
   </div>
 </div>
-               </div>
               <div className='mt-16'>
               {
                 recipes.map(recipe=><RecipeCart key={recipe.id}recipe={recipe}></RecipeCart>)
--- src/pages/home/ChefInfo.jsx
+++ src/pages/home/ChefInfo.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { FaHeart } from "react-icons/fa";
 
 const ChefInfo = ({chef}) => {
-    const {name,experience,picture,recipes,id} = chef;
+    const {name,experience,picture,recipes,id,likes} = chef;
     return (
         <div className='md:mt-16 md:px-14 lg:mt-16 lg:px-14 mt-5'>
            
@@ -15,7 +15,7 @@ const ChefInfo = ({chef}) => {
     <h2 className="card-title">{name}</h2>
     <p>Experience:{experience} years</p>
     <p>Total Recipes:{recipes} items</p>
-    <p><FaHeart></FaHeart></p>
+    <p className='flex'>Likes:<FaHeart className='text-2xl mr-2 ml-2'></FaHeart> {likes}</p>
     <div className="card-actions">
       <Link to={`/chefs/${chef.id}`}><button className="btn btn-primary">details</button></Link>
     </div>
